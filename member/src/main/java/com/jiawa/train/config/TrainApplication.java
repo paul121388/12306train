279a2746575d45242e959b7cc49ba7b1cfe3d0a5
2265370493@qq.com <102779304+paul121388@users.noreply.github.com>
2024-07-04 21:23:44 +0800

    初始化

--- member/src/main/java/com/jiawa/train/config/TrainApplication.java
+++ member/src/main/java/com/jiawa/train/config/TrainApplication.java
@@ -1,9 +1,11 @@
-package com.jiawa.train;
+package com.jiawa.train.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.jiawa"})
 public class TrainApplication {
 
     public static void main(String[] args) {
--- member/src/main/java/com/jiawa/train/controller/TestController.java
+++ member/src/main/java/com/jiawa/train/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.jiawa.train;
+package com.jiawa.train.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
