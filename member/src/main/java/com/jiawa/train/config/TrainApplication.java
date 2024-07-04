dcaf251a015b3cc6b8be0f90b0bcc71a3acc390e
2265370493@qq.com <102779304+paul121388@users.noreply.github.com>
2024-07-04 21:59:26 +0800

    4.4 修改启动日志，增加/member，修改logback日志

--- member/src/main/java/com/jiawa/train/config/TrainApplication.java
+++ member/src/main/java/com/jiawa/train/config/TrainApplication.java
@@ -1,15 +1,23 @@
 package com.jiawa.train.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.jiawa"})
 public class TrainApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(TrainApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(TrainApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(TrainApplication.class, args);
+        ConfigurableEnvironment environment = run.getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址:\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
     }
 
 }
