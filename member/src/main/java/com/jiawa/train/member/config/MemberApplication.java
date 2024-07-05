0e0c5712291fa99378e69880add796f5033a6049
2265370493@qq.com <102779304+paul121388@users.noreply.github.com>
2024-07-05 20:35:02 +0800

    4.13 继承Mybatis持久层，访问数据库成功

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -1,5 +1,6 @@
-package com.jiawa.train.config;
+package com.jiawa.train.member.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +11,7 @@ import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.jiawa"})
+@MapperScan("com.jiawa.train.member.mapper")
 public class MemberApplication {
     private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
