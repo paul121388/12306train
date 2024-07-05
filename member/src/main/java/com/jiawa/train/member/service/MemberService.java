34ef5c54e0e8dc187b10e17e893ba29a4e598bef
2265370493@qq.com <102779304+paul121388@users.noreply.github.com>
2024-07-05 21:33:07 +0800

    4.15 实现一个简单的注册接口

--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -1,8 +1,12 @@
 package com.jiawa.train.member.service;
 
+import com.jiawa.train.member.domain.Member;
+import com.jiawa.train.member.domain.MemberExample;
 import com.jiawa.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,5 +16,24 @@ public class MemberService {
 
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(String mobile) {
+        // 1. 判断手机号是否已经注册过
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+
+        List<Member> members = memberMapper.selectByExample(memberExample);
+        if(!members.isEmpty()){
+            throw new RuntimeException("手机号已经注册过");
+        }
+
+        // 2. 注册新的手机号
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
