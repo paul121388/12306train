7606f0e5018cf4b6bedf11860adbd8921d00703b
2265370493@qq.com <102779304+paul121388@users.noreply.github.com>
2024-07-06 14:04:56 +0800

    4.16 封装请求参数和返回结果

--- member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.jiawa.train.member.controller;
 
+import com.jiawa.train.CommonResp.CommonResp;
+import com.jiawa.train.member.req.MemberRegisterReq;
 import com.jiawa.train.member.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,17 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/member")
 public class MemberController {
-    @Autowired
+    @Resource
     private MemberService memberService;
 
     @GetMapping("/count")
-    public Integer count() {
-        return memberService.count();
+    public CommonResp count() {
+        Integer count = memberService.count();
+        return new CommonResp(count);
     }
 
     @PostMapping("/register")
-    public Long register(String mobile) {
-        return memberService.register(mobile);
+    public CommonResp register(MemberRegisterReq memberRegisterReq) {
+        Long id = memberService.register(memberRegisterReq);
+        return new CommonResp(id);
     }
 
 
--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.jiawa.train.member.service;
 import com.jiawa.train.member.domain.Member;
 import com.jiawa.train.member.domain.MemberExample;
 import com.jiawa.train.member.mapper.MemberMapper;
+import com.jiawa.train.member.req.MemberRegisterReq;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,9 @@ public class MemberService {
         return Math.toIntExact(memberMapper.countByExample(null));
     }
 
-    public long register(String mobile) {
+    public Long register(MemberRegisterReq memberRegisterReq) {
+        // 0. 获取手机号
+        String mobile = memberRegisterReq.getMobile();
         // 1. 判断手机号是否已经注册过
         MemberExample memberExample = new MemberExample();
         memberExample.createCriteria().andMobileEqualTo(mobile);
