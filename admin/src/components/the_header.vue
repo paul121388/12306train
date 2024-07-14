c7c8fe094d14cc36462d3bb830f4a57a7d178d3a
2265370493@qq.com <102779304+paul121388@users.noreply.github.com>
2024-07-14 19:50:42 +0800

    9.3 创建admin模块，所有的代码都是从web复制

---
+++ admin/src/components/the_header.vue
@@ -0,0 +1,58 @@
+<template>
+  <a-layout-header class="header" id="#components-layout-demo-top-side-2 ">
+    <div class="logo"/>
+    <div style="float: right; color: white">
+      您好：{{member.mobile}} &nbsp;&nbsp;
+      <router-link to="/login" style="color: white">退出</router-link>
+    </div>
+    <a-menu
+        v-model:selectedKeys="selectedKeys"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome">
+          <coffee-outlined /> &nbsp; 欢迎
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger">
+          <user-outlined /> &nbsp; 乘车人管理
+        </router-link>
+      </a-menu-item>
+
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script>
+import { defineComponent, ref ,watch} from 'vue';
+import store from "@/store";
+import router from "@/router";
+export default defineComponent({
+  name: "the_header_view",
+
+
+  components: {
+  },
+  setup() {
+    let member = store.state.member;
+    const selectedKeys = ref([]);
+    watch(() => router.currentRoute.value.path, (newValue) => {
+      console.log('watch', newValue);
+      selectedKeys.value = [];
+      selectedKeys.value.push(newValue);
+    }, {immediate: true});
+
+    return {
+      selectedKeys,
+      member
+    };
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
---
+++ admin/src/main.js
@@ -0,0 +1,51 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Antd, {notification} from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import * as Icons from '@ant-design/icons-vue';
+import axios from "axios";
+import  './assets/js/enums';
+
+const app = createApp(App)
+app.use(Antd).use(store).use(router).mount('#app')
+
+const icons = Icons;
+for(const i in icons){
+    app.component(i, icons[i])
+}
+
+axios.interceptors.request.use(function (config){
+    console.log('请求参数: ',config);
+    const _token = store.state.member.token;
+    if(_token){
+        config.headers.token = _token;
+        console.log("请求headers增加token", _token);
+    }
+    return config;
+}, error => {
+    // console.log('请求失败: ',error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(function (response){
+    console.log('返回结果: ', response);
+    return response;
+}, error => {
+    console.log('返回错误: ',error);
+    const response = error.response;
+    const status = response.status;
+    if(status === 401){
+        console.log('未登录或超时，跳转至登录页');
+        // 将原来store中信息清空
+        store.commit("setMember",{});
+        notification.error({description:"未登录或登录超时"});
+        router.push("/login");
+    }
+    return Promise.reject(error);
+});
+
+axios.defaults.baseURL = process.env.VUE_APP_URL;
+console.log('环境：', process.env.NODE_ENV);
+console.log('服务端：', process.env.VUE_APP_URL);
