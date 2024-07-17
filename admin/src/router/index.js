8e6af843ff0b3dd696bc26588bfb179de374d0d1
2265370493@qq.com <102779304+paul121388@users.noreply.github.com>
2024-07-17 20:50:14 +0800

    10.8 对菜单作分类，改为两级菜单，修改路由，改为两级路由

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -6,33 +6,43 @@ const routes = [
         path: '/',
         name: 'main-center',
         component: () => import(/* webpackChunkName: "about" */ '../views/main-center.vue'),
-        children: [{
-            path: 'welcome',
-            component: () => import('../views/main/welcome.vue'),
+        children: [
+            {
+                path: 'welcome',
+                component: () => import('../views/main/welcome.vue'),
             },
             {
                 path: 'about',
                 component: () => import('../views/main/about.vue'),
             },
             {
-                path: 'station',
-                component: () => import('../views/main/business/station.vue'),
+                path: 'base/',
+                children: [
+                    {
+                        path: 'station',
+                        component: () => import('../views/main/business/station.vue'),
+                    },
+                    {
+                        path: 'train',
+                        component: () => import('../views/main/business/train.vue'),
+                    },
+                    {
+                        path: 'train-station',
+                        component: () => import('../views/main/business/train-station.vue'),
+                    },
+                    {
+                        path: 'train-carriage',
+                        component: () => import('../views/main/business/train-carriage.vue'),
+                    },
+                    {
+                        path: 'train-seat',
+                        component: () => import('../views/main/business/train-seat.vue'),
+                    },]
             },
+
             {
-                path: 'train',
-                component: () => import('../views/main/business/train.vue'),
-            },
-            {
-                path: 'train-station',
-                component: () => import('../views/main/business/train-station.vue'),
-            },
-            {
-                path: 'train-carriage',
-                component: () => import('../views/main/business/train-carriage.vue'),
-            },
-            {
-                path: 'train-seat',
-                component: () => import('../views/main/business/train-seat.vue'),
+                path: 'batch/job',
+                component: () => import('../views/main/batch/job.vue')
             }
 
 
