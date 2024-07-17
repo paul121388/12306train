482c525f8e9f0ea7a20e41c8aaffb7697f6576d3
2265370493@qq.com <102779304+paul121388@users.noreply.github.com>
2024-07-17 16:46:46 +0800

    10.6 禁止任务并发执行

--- batch/src/main/java/com/jiawa/train/batch/job/TestJob.java
+++ batch/src/main/java/com/jiawa/train/batch/job/TestJob.java
@@ -1,12 +1,25 @@
 package com.jiawa.train.batch.job;
 
+import cn.hutool.core.date.DateTime;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+/**
+ * 禁止并发执行
+ */
+@DisallowConcurrentExecution
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob");
+        DateTime now = DateTime.now();
+        System.out.println("TestJob开始"+now.toString());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("TestJob结束"+now.toString());
     }
 }
