328959b0eb91032a9a54bec16723d2bc9ea36ddb
2265370493@qq.com <102779304+paul121388@users.noreply.github.com>
2024-07-22 15:48:17 +0800

    12.8 订单查询页面，显示当前member下的乘客复选框

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -15,7 +15,11 @@
         <span class="order-train-ticket-main">{{ item.count }}</span>&nbsp;张票&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       </span>
     </div>
-    <a-divider>{{ passengers }}</a-divider>
+    <a-divider></a-divider>
+    <b>勾选要购票的乘客：</b>&nbsp;
+    <a-checkbox-group v-model:value="passengerChecks" :options="passengerOptions"/>
+    <br/>
+    选中的乘客：{{ passengerChecks }}
   </div>
 </template>
 
@@ -29,6 +33,8 @@ export default defineComponent({
   name: "order-view",
   setup() {
     const passengers = ref([]);
+    const passengerOptions = ref([]);
+    const passengerChecks = ref([]);
     const dailyTrainTicket = SessionStorage.get(SESSION_ORDER) || {};
     console.log("下单的车次信息", dailyTrainTicket);
 
@@ -63,10 +69,10 @@ export default defineComponent({
         let data = response.data;
         if (data.success) {
           passengers.value = data.content;
-          // passengers.value.forEach((item) => passengerOptions.value.push({
-          //   label: item.name,
-          //   value: item
-          // }))
+          passengers.value.forEach((item) => passengerOptions.value.push({
+            label: item.name,
+            value: item.id
+          }))
         } else {
           notification.error({description: data.message});
         }
@@ -81,7 +87,9 @@ export default defineComponent({
       dailyTrainTicket,
       seatTypes,
       handleQueryPassenger,
-      passengers
+      passengers,
+      passengerOptions,
+      passengerChecks
     };
   }
 })
