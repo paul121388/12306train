12490e4c33e5aadd0a70adb29cefc412da514c4e
2265370493@qq.com <102779304+paul121388@users.noreply.github.com>
2024-07-22 15:16:36 +0800

    12.7 预定页面显示余票数和票价

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="order-train">
-    <span class="order-train-main">{{dailyTrainTicket.date}}</span>&nbsp;
-    <span class="order-train-main">{{dailyTrainTicket.trainCode}}</span>次&nbsp;
-    <span class="order-train-main">{{dailyTrainTicket.start}}</span>站
-    <span class="order-train-main">({{dailyTrainTicket.startTime}})</span>&nbsp;
+    <span class="order-train-main">{{ dailyTrainTicket.date }}</span>&nbsp;
+    <span class="order-train-main">{{ dailyTrainTicket.trainCode }}</span>次&nbsp;
+    <span class="order-train-main">{{ dailyTrainTicket.start }}</span>站
+    <span class="order-train-main">({{ dailyTrainTicket.startTime }})</span>&nbsp;
     <span class="order-train-main">——</span>&nbsp;
-    <span class="order-train-main">{{dailyTrainTicket.end}}</span>站
-    <span class="order-train-main">({{dailyTrainTicket.endTime}})</span>&nbsp;
+    <span class="order-train-main">{{ dailyTrainTicket.end }}</span>站
+    <span class="order-train-main">({{ dailyTrainTicket.endTime }})</span>&nbsp;
+
+    <div class="order-train-ticket">
+      <span v-for="item in seatTypes" :key="item.type">
+        <span>{{ item.desc }}</span>：
+        <span class="order-train-ticket-main">{{ item.price }}￥</span>&nbsp;
+        <span class="order-train-ticket-main">{{ item.count }}</span>&nbsp;张票&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </span>
+    </div>
+
   </div>
 </template>
 
@@ -18,8 +27,36 @@ export default defineComponent({
   setup() {
     const dailyTrainTicket = SessionStorage.get(SESSION_ORDER) || {};
     console.log("下单的车次信息", dailyTrainTicket);
+
+    const SEAT_TYPE = window.SEAT_TYPE;
+    console.log(SEAT_TYPE)
+    // 本车次提供的座位类型seatTypes，含票价，余票等信息，例：
+    // {
+    //   type: "YDZ",
+    //   code: "1",
+    //   desc: "一等座",
+    //   count: "100",
+    //   price: "50",
+    // }
+    // 关于SEAT_TYPE[KEY]：当知道某个具体的属性xxx时，可以用obj.xxx，当属性名是个变量时，可以使用obj[xxx]
+    const seatTypes = [];
+    for (let KEY in SEAT_TYPE) {
+      let key = KEY.toLowerCase();
+      if (dailyTrainTicket[key] >= 0) {
+        seatTypes.push({
+          type: KEY,
+          code: SEAT_TYPE[KEY]["code"],
+          desc: SEAT_TYPE[KEY]["desc"],
+          count: dailyTrainTicket[key],
+          price: dailyTrainTicket[key + 'Price'],
+        })
+      }
+    }
+    console.log("本车次提供的座位：", seatTypes)
+
     return {
-      dailyTrainTicket
+      dailyTrainTicket,
+      seatTypes
     };
   }
 })
@@ -30,9 +67,11 @@ export default defineComponent({
   font-size: 18px;
   font-weight: bold;
 }
+
 .order-train .order-train-ticket {
   margin-top: 15px;
 }
+
 .order-train .order-train-ticket .order-train-ticket-main {
   color: red;
   font-size: 18px;
@@ -41,9 +80,11 @@ export default defineComponent({
 .order-tickets {
   margin: 10px 0;
 }
+
 .order-tickets .ant-col {
   padding: 5px 10px;
 }
+
 .order-tickets .order-tickets-header {
   background-color: cornflowerblue;
   border: solid 1px cornflowerblue;
@@ -51,6 +92,7 @@ export default defineComponent({
   font-size: 16px;
   padding: 5px 0;
 }
+
 .order-tickets .order-tickets-row {
   border: solid 1px cornflowerblue;
   border-top: none;
