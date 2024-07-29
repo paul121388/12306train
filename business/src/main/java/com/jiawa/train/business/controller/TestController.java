a08269be62680e48dcb38f15ed0b06f631a7bec7
2265370493@qq.com <102779304+paul121388@users.noreply.github.com>
2024-07-29 17:59:09 +0800

    17.7 Sentinel限流规则 关联

--- business/src/main/java/com/jiawa/train/business/controller/TestController.java
+++ business/src/main/java/com/jiawa/train/business/controller/TestController.java
@@ -1,12 +1,21 @@
 package com.jiawa.train.business.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping("/hello")
-    public String test() {
+    @SentinelResource("hello2")
+    @GetMapping("/hello2")
+    public String hello() throws InterruptedException {
+        Thread.sleep(500);
+        return "Hello World! Business";
+    }
+
+    @SentinelResource("hello1")
+    @GetMapping("/hello1")
+    public String hello1() {
         return "Hello World! Business";
     }
 }
