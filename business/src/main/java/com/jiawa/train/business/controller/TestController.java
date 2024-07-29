cde131fef95b3a72b4bb51c175fbad5790e42dc0
2265370493@qq.com <102779304+paul121388@users.noreply.github.com>
2024-07-29 18:27:03 +0800

    17.8 Sentinel熔断

--- business/src/main/java/com/jiawa/train/business/controller/TestController.java
+++ business/src/main/java/com/jiawa/train/business/controller/TestController.java
@@ -7,15 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @SentinelResource("hello2")
-    @GetMapping("/hello2")
+    @GetMapping("/hello")
     public String hello() throws InterruptedException {
         Thread.sleep(500);
         return "Hello World! Business";
     }
 
-    @SentinelResource("hello1")
-    @GetMapping("/hello1")
-    public String hello1() {
-        return "Hello World! Business";
-    }
+
 }
